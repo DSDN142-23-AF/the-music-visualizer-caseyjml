7c1e6afb0f56a0f4836966178c0ece88bf07854d
caseyjml <151981877+caseyjml@users.noreply.github.com>
2024-02-08 21:48:04 +1300

    intro

--- music_starter.js
+++ music_starter.js
@@ -1,8 +1,6 @@
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 
-circleSize = 5
-
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
   
   colorMode(RGB, 60);
@@ -12,13 +10,20 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   noFill();
 
 
+// OPENING SCENE
+if(counter < 10){
+let drumMap = map(drum, 0, 100, 5, 150);
 
-  if(counter > 15){
-    fill(0);
-    ellipse(width/2, height/2, circleSize)
-
-    circleSize = circleSize + 10
+  stroke(drumMap, 80, 80);
+  for(let l = 1; l <= 47; l++){
+   let ellipseStep = 20*l // space between lines
+    ellipse(ellipseStep, height/2, 1, drumMap)
   }
+}
+  
+
+// SCENE 2
+  if(counter > 10){
   
   c1 = color(0); // black
   c2 = color(156, 25, 25); // red
@@ -32,18 +37,6 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
     line(0,y,width, y); // drawing the gradient (line the width of the screen)
   }
 
-  //let drumMap = map(drum, 0, 100, 5, 30);
-
-  //let lineLength = 300;
-  //let lineStart = 100;
-  //let lineEnd = lineStart + lineLength;
-
-
-  //stroke(drumMap, 80, 80);
-  //for(let iii = 1; iii <= drumMap; iii++){
-   // let lineStep = iii*20
-    //(lineStart, lineStep, lineEnd, lineStep);
-  //}
 
   let bassMap = map(vocal, 0, 100, 10, 130);
   //stroke(bassMap, 80, 80);
@@ -66,5 +59,6 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   for(let o = 1; o <= otherMap; o++){
     let oGap = o*20; // gap size between circles
     ellipse(width/2, height/2, oGap);
+  }
   }
 }
