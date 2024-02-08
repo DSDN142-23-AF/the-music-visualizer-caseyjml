b24976daf2fe795997cc1d9480399c43501abddd
caseyjml <151981877+caseyjml@users.noreply.github.com>
2024-02-08 18:46:13 +1300

    added circles (other map)

--- music_starter.js
+++ music_starter.js
@@ -2,30 +2,41 @@
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
   
-  colorMode(HSB, 60);
+  colorMode(RGB, 60);
   background(0);
   rectMode(CENTER);
   strokeWeight(10);
   noFill();
 
+  c1 = color(0); // black
+  c2 = color(156, 25, 25); // red
+  
+  let gradientMap = map(bass, 0, 100, 0, 2); 
 
-  let drumMap = map(drum, 0, 100, 5, 30);
-
-  let lineLength = 300;
-  let lineStart = 100;
-  let lineEnd = lineStart + lineLength;
-
-
-  stroke(drumMap, 80, 80);
-  for(let iii = 1; iii <= drumMap; iii++){
-    let lineStep = iii*20
-    line(lineStart, lineStep, lineEnd, lineStep);
+  for(let y=0; y<height; y++){
+    n = map(y,0,height,0,gradientMap);
+    let newc = lerpColor(c1,c2,n); // new colour gradient 
+    stroke(newc);
+    line(0,y,width, y); // drawing the gradient (line the width of the screen)
   }
-   
+
+  //let drumMap = map(drum, 0, 100, 5, 30);
+
+  //let lineLength = 300;
+  //let lineStart = 100;
+  //let lineEnd = lineStart + lineLength;
+
+
+  //stroke(drumMap, 80, 80);
+  //for(let iii = 1; iii <= drumMap; iii++){
+   // let lineStep = iii*20
+    //(lineStart, lineStep, lineEnd, lineStep);
+  //}
+
   
-  let bassMap = map(bass, 0, 100, 10, 100);
+  let bassMap = map(vocal, 0, 100, 10, 100);
   stroke(bassMap, 80, 80);
-  
+
   for (let ii = 1; ii <= 10; ii++){ // bass map circles y-coord
     let yStep = ii*50;
 
@@ -33,9 +44,13 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
       ellipse(50*i, yStep, bassMap);
     }
   }
- // let ballSize = 60;
- //let bassWidth = map(bass, 0, 100, 0+ballSize/2, width);
 
- // fill(200, 0, 0);
- // ellipse(bassWidth, height/2, ballSize);
+  noFill();
+  strokeWeight(other/50); // stroke weight increases with # of circles
+  let otherMap = map(other, 0, 100, 0, 50); // other map
+
+  for(let o = 1; o <= otherMap; o++){
+    let oGap = o*20; // gap size between circles
+    ellipse(width/2, height/2, oGap);
+  }
 }
